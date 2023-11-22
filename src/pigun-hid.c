8db6762b48a46ff02f8159ea1df5d9402ecebd84
Filippo Federici <fullmetalfelix@users.noreply.github.com>
2023-11-22 23:42:43 +0200

    Update pigun-hid.c

--- src/pigun-hid.c
+++ src/pigun-hid.c
@@ -105,27 +105,17 @@ const uint8_t hid_descriptor_joystick_mode[] = {
 	// 45 bytes so far!
 	// now begins the new physical interface device for feedback
 
-	0x05, 0x0F,        // Usage Page (PID Page)
-	0x09,0x73,	// USAGE (Set Constant Force Report)
-	0xA1,0x02,	// COLLECTION (Logical)
-		0x85,0x05,	// REPORT_ID (05)
-		0x09,0x22,	// USAGE (Effect Block Index)
-		0x15,0x01,	// LOGICAL_MINIMUM (01)
-		0x25,0x28,	// LOGICAL_MAXIMUM (28)
-		0x35,0x01,	// PHYSICAL_MINIMUM (01)
-		0x45,0x28,	// PHYSICAL_MAXIMUM (28)
-		0x75,0x08,	// REPORT_SIZE (08)
-		0x95,0x01,	// REPORT_COUNT (01)
-		0x91,0x02,	// OUTPUT (Data,Var,Abs)
-		0x09,0x70,	// USAGE (Magnitude)
-		0x16,0x01,0xFF,	// LOGICAL_MINIMUM (-255)
-		0x26,0xFF,0x00,	// LOGICAL_MAXIMUM (255)
-		0x36,0xF0,0xD8,	// PHYSICAL_MINIMUM (-10000)
-		0x46,0x10,0x27,	// PHYSICAL_MAXIMUM (10000)
-		0x75,0x10,	// REPORT_SIZE (10)
-		0x95,0x01,	// REPORT_COUNT (01)
-		0x91,0x02,	// OUTPUT (Data,Var,Abs)
-	0xC0,	// END COLLECTION ()
+  0x06, 0x00, 0xFF,            // (GLOBAL) USAGE_PAGE         0xFF00 Vendor-defined 
+  0xA1, 0x01,                  // (MAIN)   COLLECTION         0x01 Application (Usage=0x0: Page=, Usage=, Type=) <-- Warning: USAGE type should be CA (Application)
+  0x85, 0x02,                  //   (GLOBAL) REPORT_ID          0x02 (2) 
+  0x09, 0x01,                  //   (LOCAL)  USAGE              0xFF000001  
+  0x15, 0x00,                  //   (GLOBAL) LOGICAL_MINIMUM    0x00 (0) <-- Redundant: LOGICAL_MINIMUM is already 0
+  0x26, 0xFF, 0x00,            //   (GLOBAL) LOGICAL_MAXIMUM    0x00FF (255) 
+  0x75, 0x08,                  //   (GLOBAL) REPORT_SIZE        0x08 (8) Number of bits per field 
+  0x95, 0x40,                  //   (GLOBAL) REPORT_COUNT       0x40 (1) Number of fields 
+  0x09, 0x01,                  //   (LOCAL)  USAGE              0xFF000001  
+  0x91, 0x02,                  //   (MAIN)   OUTPUT             0x00000002 (64 fields x 8 bits) 0=Data 1=Variable 0=Absolute 0=NoWrap 0=Linear 0=PrefState 0=NoNull 0=NonVolatile 0=Bitmap 
+  0xC0,                        // (MAIN)   END_COLLECTION     Application
 
 /*
 0x05, 0x0F,        // Usage Page (PID Page)
