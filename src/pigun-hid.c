4f263929959cac36df6d99bf6a5b8c6761e49d3f
Filippo Federici <fullmetalfelix@users.noreply.github.com>
2023-11-24 15:21:29 +0200

    Update pigun-hid.c

--- src/pigun-hid.c
+++ src/pigun-hid.c
@@ -108,18 +108,6 @@ const uint8_t hid_descriptor_joystick_mode[] = {
 	0xc0,                    //             END_COLLECTION
 	0xc0,                    //       END_COLLECTION
 
- 
- //           0x05, 0x01,            //            USAGE_PAGE (Generic Desktop) 
- //           0x09, 0x00,            //            USAGE (Undefined) 
-            0x06, 0x00, 0xFF,      //            USAGE_PAGE (Vendor Defined Page 1) 
-            0x09, 0x01,            //            USAGE (Vendor Usage 1) 
-            0x15, 0x00,            //            LOGICAL_MINIMUM (0) 
-            0x26, 0xff, 0x00,      //            LOGICAL_MAXIMUM (255) 
-            0x75, 0x08,            //            REPORT_SIZE (8) 
-            0x95, 0x40,            //            REPORT_COUNT (64) 
-            0x91, 0x02,            //            OUTPUT (Data,Var,Abs)        // <-------- 
- //           0xB1, 0x02,            //            FEATURE (Data,Var,Abs)     // <-------- 
-
 
 	0x06, 0x00, 0xFF,       // USAGE_PAGE (Vendor Defined Page 1) 
 	0x09, 0x01,            //            USAGE (Vendor Usage 1) 
