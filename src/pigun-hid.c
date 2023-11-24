4cea587b67b6c0e29d713bd4e467a1ccce9df814
Filippo Federici <fullmetalfelix@users.noreply.github.com>
2023-11-24 18:21:46 +0200

    Update pigun-hid.c

--- src/pigun-hid.c
+++ src/pigun-hid.c
@@ -80,6 +80,45 @@ const uint8_t hid_descriptor_joystickWORKNOFFB_mode[] = {
 };
 
 
+const uint8_t hid_descriptor_joystick_mode[] = {
+	0x05, 0x01,        // Usage Page (Generic Desktop Ctrls)
+	0x09, 0x04,        // Usage (Joystick)
+	0xA1, 0x01,        // Collection (Application)
+		0x09, 0x01,        //   Usage (Pointer)
+		0xA1, 0x00,        //   Collection (Physical)
+			0x85, 0x01,		   // 	Report ID 1
+
+			0x05, 0x01,        // Usage Page (Generic Desktop Ctrls)
+			0x16, 0x01, 0x80,  //   Logical Minimum 0x8001 (-32767)  
+			0x26, 0xFF, 0x7F,  //   Logical Maximum 0x7FFF (32767)
+			0x09, 0x30,        //     Usage (X)
+			0x09, 0x31,        //     Usage (Y)
+			0x75, 0x10,        //     Report Size (16)
+			0x95, 0x02,        //     Report Count (2)
+			0x81, 0x02,        //     Input (Data,Var,Abs,No Wrap,Linear,Preferred State,No Null Position)
+
+			0x05, 0x09,        //   Usage Page (Button)
+			0x19, 0x01,        //   Usage Minimum (0x01)
+			0x29, 0x08,        //   Usage Maximum (0x08)
+			0x15, 0x00,        //   Logical Minimum (0)
+			0x25, 0x01,        //   Logical Maximum (1)
+			0x75, 0x01,        //   Report Size (1)
+			0x95, 0x08,        //   Report Count (8)
+			0x81, 0x02,        //   Input (Data,Var,Abs,No Wrap,Linear,Preferred State,No Null Position)
+
+
+			0x09, 0x03,		  	// usage ID vendor defined
+			0x15, 0x00,			// Logical Minimum (0)
+			0x26, 0xFF, 0x00   	// Logical Maximum (1)
+			0x75, 0x08,        	// Report Size (8)
+			0x95, 0x01,        	// Report Count (1)
+			0x91, 0x02,			// output (data,Var,Abs)
+
+		0xC0,              //   End Collection   --- 27 bytes
+	0xC0              // End Collection --- 44 bytes
+};
+
+
 
 // this is not seen a game controller by windows
 const uint8_t hid_descriptor_joystick0_mode[] = {
@@ -121,8 +160,8 @@ const uint8_t hid_descriptor_joystick0_mode[] = {
 
 };
 
-
-const uint8_t hid_descriptor_joystick_mode[] = {
+/* // works but windows does not think it is a FFB capable device
+const uint8_t hid_descriptor_joystick1_mode[] = {
 	0x05, 0x01,        // Usage Page (Generic Desktop Ctrls)
 	0x09, 0x04,        // Usage (Joystick)
 	0xA1, 0x01,        // Collection (Application)
@@ -760,7 +799,7 @@ const uint8_t hid_descriptor_joystick_mode[] = {
 	
 	0xC0               // End Collection (Application)
 }; // 137 bytes total
-
+*/
 
 
 // STATE
