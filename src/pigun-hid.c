ba9cfeb1d2ee9f5ffa5c34f63691a9e35e3fa04c
Filippo Federici <fullmetalfelix@users.noreply.github.com>
2023-11-24 15:12:01 +0200

    Update pigun-hid.c

--- src/pigun-hid.c
+++ src/pigun-hid.c
@@ -84,29 +84,29 @@ const uint8_t hid_descriptor_joystickWORKNOFFB_mode[] = {
 
 const uint8_t hid_descriptor_joystick_mode[] = {
 
-	0x09, 0x04,              // USAGE (Joystick)
-	0xa1, 0x01,              // COLLECTION (Application)
-	0xa1, 0x01,              //      COLLECTION (Application)
-	0x09, 0x01,              //             USAGE (Pointer)
-	0xa1, 0x00,              //             COLLECTION (Physical)
-	0x05, 0x01,              //                   USAGE_PAGE (Generic Desktop)
-	0x09, 0x33,              //                   USAGE (Rx)
-	0x09, 0x34,              //                   USAGE (Ry)
+	0x09, 0x04,              // USAGE (Joystick)
+	0xa1, 0x01,              // COLLECTION (Application)
+	0xa1, 0x01,              //      COLLECTION (Application)
+	0x09, 0x01,              //             USAGE (Pointer)
+	0xa1, 0x00,              //             COLLECTION (Physical)
+	0x05, 0x01,              //                   USAGE_PAGE (Generic Desktop)
+	0x09, 0x33,              //                   USAGE (Rx)
+	0x09, 0x34,              //                   USAGE (Ry)
 	0x16, 0x01, 0x80,  		//   Logical Minimum 0x8001 (-32767)  
 	0x26, 0xFF, 0x7F,  		//   Logical Maximum 0x7FFF (32767)
-	0x75, 0x16,              //                   REPORT_SIZE (10)
-	0x95, 0x02,              //                   REPORT_COUNT (2)
-	0x81, 0x02,              //                   INPUT (Data,Var,Abs)
-	0x05, 0x09,              //                   USAGE_PAGE (Button)
-	0x19, 0x01,              //                   USAGE_MINIMUM (Button 1)
-	0x29, 0x08,              //                   USAGE_MAXIMUM (Button 23)
-	0x15, 0x00,              //                   LOGICAL_MINIMUM (0)
-	0x25, 0x01,              //                   LOGICAL_MAXIMUM (1)
-	0x75, 0x01,              //                   REPORT_SIZE (1)
-	0x95, 0x08,              //                   REPORT_COUNT (8)
-	0x81, 0x02,              //                   INPUT (Data,Var,Abs)
-	0xc0,                    //             END_COLLECTION
-	0xc0,                    //       END_COLLECTION
+	0x75, 0x16,              //                   REPORT_SIZE (10)
+	0x95, 0x02,              //                   REPORT_COUNT (2)
+	0x81, 0x02,              //                   INPUT (Data,Var,Abs)
+	0x05, 0x09,              //                   USAGE_PAGE (Button)
+	0x19, 0x01,              //                   USAGE_MINIMUM (Button 1)
+	0x29, 0x08,              //                   USAGE_MAXIMUM (Button 23)
+	0x15, 0x00,              //                   LOGICAL_MINIMUM (0)
+	0x25, 0x01,              //                   LOGICAL_MAXIMUM (1)
+	0x75, 0x01,              //                   REPORT_SIZE (1)
+	0x95, 0x08,              //                   REPORT_COUNT (8)
+	0x81, 0x02,              //                   INPUT (Data,Var,Abs)
+	0xc0,                    //             END_COLLECTION
+	0xc0,                    //       END_COLLECTION
 
  
  //           0x05, 0x01,            //            USAGE_PAGE (Generic Desktop) 
@@ -121,15 +121,15 @@ const uint8_t hid_descriptor_joystick_mode[] = {
  //           0xB1, 0x02,            //            FEATURE (Data,Var,Abs)     // <-------- 
 
 
-	0x06, 0x00, 0xFF,       // USAGE_PAGE (Vendor Defined Page 1) 
+	0x06, 0x00, 0xFF,       // USAGE_PAGE (Vendor Defined Page 1) 
 	0x09, 0x01,            //            USAGE (Vendor Usage 1) 
-	0x15, 0x00,              // LOGICAL_MINIMUM (0)
-	0x26, 0xff, 0x00,        // LOGICAL_MAXIMUM (255)
-	0x75, 0x08,              // REPORT_SIZE (8)
-	0x95, 0x01,              // REPORT_COUNT (1)
-	0xb2, 0x03, 0x01,        // FEATURE (Cnst,Var,Abs,Buf)
+	0x15, 0x00,              // LOGICAL_MINIMUM (0)
+	0x26, 0xff, 0x00,        // LOGICAL_MAXIMUM (255)
+	0x75, 0x08,              // REPORT_SIZE (8)
+	0x95, 0x01,              // REPORT_COUNT (1)
+	0xb2, 0x03, 0x01,        // FEATURE (Cnst,Var,Abs,Buf)
 
-	0xc0                     // END_COLLECTION
+	0xc0                     // END_COLLECTION
 
 };
 
