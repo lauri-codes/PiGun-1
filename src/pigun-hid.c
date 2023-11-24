fe932f050bb75707eb4b7ad98c471785ebc20e12
Filippo Federici <fullmetalfelix@users.noreply.github.com>
2023-11-24 13:28:01 +0200

    Update pigun-hid.c

--- src/pigun-hid.c
+++ src/pigun-hid.c
@@ -84,33 +84,28 @@ const uint8_t hid_descriptor_joystick_mode[] = {
 
 const uint8_t hid_descriptor_joystick_mode[] = {
 
-0x05,0x01,	// USAGE_PAGE (Generic Desktop)
-0x09,0x04,	// USAGE (Joystick)
-0xA1,0x01,	// COLLECTION (Application)
-	0x85,0x01,	// REPORT_ID (1)
-
-	// FFP input descriptor:
-   	0x09, 0x01,        //   Usage (Pointer)
-    0xA1, 0x00,        //   Collection (Physical)
+0x05, 0x01,        // Usage Page (Generic Desktop Ctrls)
+0x09, 0x04,        // Usage (Joystick)
+0xA1, 0x01,        // Collection (Application)
+	0x16, 0x01, 0x80,  //   Logical Minimum 0x8001 (-32767)  
+	0x26, 0xFF, 0x7F,  //   Logical Maximum 0x7FFF (32767)
+	0x09, 0x01,        //   Usage (Pointer)
+	0xA1, 0x00,        //   Collection (Physical)
 		0x09, 0x30,        //     Usage (X)
 		0x09, 0x31,        //     Usage (Y)
-		0x16, 0x01, 0x80,  //   Logical Minimum 0x8001 (-32767)  
-		0x26, 0xFF, 0x7F,  //   Logical Maximum 0x7FFF (32767)
 		0x75, 0x10,        //     Report Size (16) --- 22 bytes
 		0x95, 0x02,        //     Report Count (2)
-        0x81, 0x02,        //     Input (Data,Var,Abs,No Wrap,Linear,Preferred State,No Null Position)
-	0xC0,              // End Collection
-
+		0x81, 0x02,        //     Input (Data,Var,Abs,No Wrap,Linear,Preferred State,No Null Position)
+	0xC0,              //   End Collection   --- 27 bytes
 	0x05, 0x09,        //   Usage Page (Button)
 	0x19, 0x01,        //   Usage Minimum (0x01)
 	0x29, 0x08,        //   Usage Maximum (0x08)
 	0x15, 0x00,        //   Logical Minimum (0)
 	0x25, 0x01,        //   Logical Maximum (1) --- 37 bytes
-	0x35, 0x00,		   //   PHYSICAL_MINIMUM (0)
-	0x45, 0x01,		   //   PHYSICAL_MAXIMUM (1)
 	0x75, 0x01,        //   Report Size (1)
 	0x95, 0x08,        //   Report Count (8)
 	0x81, 0x02,        //   Input (Data,Var,Abs,No Wrap,Linear,Preferred State,No Null Position)
+	
 /*
 	0x55, 0x00, 	// ( UNIT_EXPONENT ( 0))
 	0x65, 0x00, 	// ( UNIT ( None))
