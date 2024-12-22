f9e4e90eea5ae25f10392b14be51f2f278894102
Lauri Himanen <lauri.himanen@gmail.com>
2024-12-22 22:02:49 +0200

    Test.

--- src/libmain.cpp
+++ src/libmain.cpp
@@ -8,8 +8,31 @@
 using namespace libcamera;
 using namespace std::chrono_literals;
 
+static std::shared_ptr<Camera> camera;
+
 int main()
 {
+    // Create and start the CameraManager
+    std::unique_ptr<CameraManager> cm = std::make_unique<CameraManager>();
+    cm->start();
+
+    // Acquire the camera
+    auto cameras = cm->cameras();
+    if (cameras.empty()) {
+        std::cout << "No cameras were identified on the system."
+                << std::endl;
+        cm->stop();
+        return EXIT_FAILURE;
+    }
+    std::string cameraId = cameras[0]->id();
+    auto camera = cm->get(cameraId);
+    camera->acquire();
+
+    // Configure camera
+    std::unique_ptr<CameraConfiguration> config = camera->generateConfiguration( { StreamRole::Viewfinder } );
+    StreamConfiguration &streamConfig = config->at(0);
+    std::cout << "Default viewfinder configuration is: " << streamConfig.toString() << std::endl;
+
     // Code to follow
     return 0;
 }
