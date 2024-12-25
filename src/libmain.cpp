8c23e438c289bf299cebc12375deefa30603b450
Lauri Himanen <lauri.himanen@gmail.com>
2024-12-25 21:37:14 +0200

    Frame processing.

--- src/libmain.cpp
+++ src/libmain.cpp
@@ -4,11 +4,18 @@
 #include <thread>
 
 #include <libcamera/libcamera.h>
+#include <libcamera/framebuffer_allocator.h>
 
 using namespace libcamera;
 using namespace std::chrono_literals;
 
 static std::shared_ptr<Camera> camera;
+
+static void requestComplete(Request *request)
+{
+    // Code to follow
+    std::cerr << "Request complete!" << std::endl;
+}
 
 int main()
 {
@@ -19,8 +26,7 @@ int main()
     // Acquire the camera
     auto cameras = cm->cameras();
     if (cameras.empty()) {
-        std::cout << "No cameras were identified on the system."
-                << std::endl;
+        std::cout << "No cameras were identified on the system." << std::endl;
         cm->stop();
         return EXIT_FAILURE;
     }
@@ -29,7 +35,7 @@ int main()
     camera->acquire();
 
     // Configure camera
-    std::unique_ptr<CameraConfiguration> config = camera->generateConfiguration( { StreamRole::Viewfinder } );
+    std::unique_ptr<CameraConfiguration> config = camera->generateConfiguration( { StreamRole::VideoRecording } );
     StreamConfiguration &streamConfig = config->at(0);
     streamConfig.size.width = 640; 
     streamConfig.size.height = 480;
@@ -39,9 +45,59 @@ int main()
         std::cerr << "Configuration invalid" << std::endl;
         return -1;
     }
+    if (camera->configure(config.get()) < 0) {
+        std::cerr << "Failed to configure camera" << std::endl;
+        return -1;
+    }
+
+    // Use a FrameBufferAllocator to allocate buffers (not fully shown)
+    std::unique_ptr<FrameBufferAllocator> allocator = std::make_unique<FrameBufferAllocator>(camera);
+    Stream *stream = streamConfig.stream();
+    if (allocator->allocate(stream) < 0) {
+        std::cerr << "Failed to allocate buffers" << std::endl;
+        return -1;
+    }
+
+    // Frame capture
+    Stream *stream = streamConfig.stream();
+    const std::vector<std::unique_ptr<FrameBuffer>> &buffers = allocator->buffers(stream);
+    std::vector<std::unique_ptr<Request>> requests;
+    for (unsigned int i = 0; i < buffers.size(); ++i) {
+        std::unique_ptr<Request> request = camera->createRequest();
+        if (!request)
+        {
+            std::cerr << "Can't create request" << std::endl;
+            return -ENOMEM;
+        }
+
+        const std::unique_ptr<FrameBuffer> &buffer = buffers[i];
+        int ret = request->addBuffer(stream, buffer.get());
+        if (ret < 0)
+        {
+            std::cerr << "Can't set buffer for request"
+                << std::endl;
+            return ret;
+        }
+
+        requests.push_back(std::move(request));
+    }
+
+    // Set callback for frame request completion
+    camera->requestCompleted.connect(requestComplete);
+
+    // Start camera and queue requests
+    camera->start();
+    for (std::unique_ptr<Request> &request : requests) {
+        camera->queueRequest(request.get());
+    }
+
+    // Wait for 3 seconds
+    std::this_thread::sleep_for(3000ms);
 
     // Stop camera and release resources
     camera->stop();
+    allocator->free(stream);
+    delete allocator;
     camera->release();
     camera.reset();
     cm->stop();
